6d8f634f4f7538c508cfc8caacca1f097cc94eca
李晨军 <firren@163.com>
2015-12-16 15:45:45 +0800

    规则修改

--- application/controllers/Active.php
+++ application/controllers/Active.php
@@ -141,14 +141,21 @@ class ActiveController extends BaseController
 //    普通奖品的抽取规则——随机，用户获得礼品的概率，根据配置的概率表来获得
 //    如：用户A获得奖品的概率表
 //    ||物品ID||获得物品的概率||
-//     ||1||80%||
-//     ||2||10%||
-//     ||3||5%||
-//     ||4||3%||
-//     ||5||1%||
-//     ||6||1%||
+//    普通奖项：每天可以中奖的物品
+//      物品ID  剩余数量  中奖概率
+//      | 01 || 3 || 1% |
+//      | 02 ||10 || 1.5% |
+//      | 03 ||12 || 2% |
+//      | 04 || 999999999 || 95.5% |
+//      如果角色A抽中的物品，剩余数量为0，则显示用户中四等奖
+//
+//      特等大奖
+//      开放时间：2015/12/24 13:14
+
         $arr = []; //获奖信息
         $prizeModel = new prizeModel();
+        $winprizeModel = new winPrizeModel();
+        $winprizelogModel = new winprizelogModel();
         $prize = $prizeModel->getActiveGoods($this->active_id, 1);
         if($prize){
             //恭喜你获取特殊大奖
@@ -158,7 +165,7 @@ class ActiveController extends BaseController
                 $arr['id'] = $prize['id'];
                 $arr['name'] = $prize['name'];
                 //添加到中奖表（因为是100%中奖）
-                $winprizeModel = new winPrizeModel();
+
                 $data =[
                     'pid'=>$arr['id'],
                     'active_id'=>$this->active_id,
@@ -166,7 +173,7 @@ class ActiveController extends BaseController
                 ];
                 $winprizeModel->addWin($data);
                 //添加到中奖日志表
-                $winprizelogModel = new winprizelogModel();
+
                 $data = [
                     'pid' => $arr['id'],
                     'aid' => $this->active_id,
@@ -176,13 +183,15 @@ class ActiveController extends BaseController
                 return $arr;
             }
         }
-        //普通奖品中抽奖
-        $prize2 = $prizeModel->getActiveGoods($this->active_id);
+        /*
+         * 普通奖品中抽奖
+         */
+        //1 提取当天奖品 1，2，3 奖品
+        $prize2 = $this->prizeAvalible();
         //抽到的数字
-        $rand_num = rand(1, 100);  //33
+        $rand_num = rand(1, 10000);  //33
         $num = 0;
         foreach ($prize2 as $key => $val) {
-
             $num += $val['probability'];
             if ($num >= $rand_num) {
                 //恭喜获取这个奖
@@ -208,24 +217,31 @@ class ActiveController extends BaseController
         ];
         $winprizelogModel->addWin($data);
         return $arr;
-//
-//     特殊大奖的获奖规则——每日定时发放
-//     如：用户B活动大奖的机制
-//    ||物品ID||开放奖品的时间||
-//     ||1||2015-12-21 23:00||
-//     ||2||2015-12-22 23:00||
-//     ||3||2015-12-23 23:00||
-//     ||4||2015-12-24 23:00||
-//     ||5||2015-12-25 23:00||
-//     ||6||2015-12-26 23:00||
-//    系统在22号晚上11:00准时开放可抽取的大奖“2”，如果此时用户B是在开放大奖后最近一个时间点
-//（如2015-12-22 23:01）提交抽奖的用户即可抽取到对应的奖品“2”；
-//如果同时有多个用户同时提交表单，则在这几个用户中随机选择一位获得奖品
-//        一个用户同时有且仅有可能获得一个奖品，获得大奖的优先级高于普通奖品。
-//        即：如果一个用户未抽取到大奖，则进入普通奖品抽取规则；如果用户获得大奖，则同一时间不会获得普通奖品
-
     }
 
+    /**
+     * @return array 获取可供中奖的奖品
+     */
+    private function prizeAvalible(){
+        $start_time = date('Y-m-d',time())." 00:00:00";
+        $end_time = date('Y-m-d',time())." 23:59:59";
+        $prizeModel = new prizeModel();
+        $winprizeModel = new winPrizeModel();
+        $prizes = $prizeModel->getActiveGoods($this->active_id);
+        $filterPrize = array();
+        if (count($prizes) > 0){
+            foreach ($prizes as $key => $prize){
+                    $winprizeModel->pid = $prize['id'];
+                    $num = $winprizeModel->fetchWinNum($start_time,$end_time);
+                    if ($num && intval($num['num']) >= intval($prize['num'])){//已经抽过了奖品
+                        continue;
+                    }else{
+                        $filterPrize[] = $prize;
+                    }
+            }
+        }
+        return $filterPrize;
+    }
     /**
      * 检查是否能抽奖
      */
