aa49d2f182631f35f74806eb747e48dfc0e50335
李晨军 <firren@163.com>
2015-12-23 18:24:09 +0800

    add

--- application/controllers/Userrole.php
+++ application/controllers/Userrole.php
@@ -8,78 +8,70 @@
  */
 class UserroleController extends BaseController
 {
+    /**
+     * 活动ID
+     * @var int
+     */
+    private $active_id = 0;
+
+    /**
+     * opend_id
+     * 微信唯一ID等
+     */
+    private $opend_id = '';
+    /**
+     * 邀请人ID
+     * @var int
+     */
+    private $inviter_id = 0;
+    /**
+     * 用户MODEL
+     */
+    private $activeUserModel;
     public function init()
     {
         parent::init();
+        $this->activeUserModel = new activeUserModel();
+        $active_id = I('active_id');
+        $type = I('type', 0);
+        if(!$active_id){
+            $this->returnJson(100);
+        }
+        $this->active_id = $active_id;
+        $this->type = $type;
     }
 
     public function loginAction()
     {
-        $state = htmlspecialchars($_GET['state']);
-        if(!$state ==$_SESSION['active']['state']){
-            echo "不要攻击我，我会报警的！";
-            exit;
+        $opend_id = I('opend_id');
+        $this->inviter_id = I('uid', 0, 'intval');
+        if(!$opend_id){
+            $this->returnJson(100);
         }
-        $active_id = intval($_GET['active_id']);
-        $type = intval($_GET['type']);
-        $inviter_id = @intval($_GET['inviter_id']);
+        $this->opend_id = $opend_id;
+        switch($this->type){
+            case 0:
+                $this->_webLogin();
+                break;
+            case 1:
+                $this->_wenxinLogin();
+                break;
+            case 2:
+                $this->_sinaLogin();
+                break;
+            case 3:
+                $this->_qqLogin();
+                break;
+            default:
+                $this->returnJson(102);
+        }
 
-        if(!isset($active_id) || !isset($type) || !$type || !$active_id){
-            $this->returnJson(100);
-        }
-        $ret = $this->_getToken();
-        if(!isset($ret['access_token'])){
-            $this->returnJson(100);
-        }
-        //token 去获取用户昵称
-        $userInfo = $this->_getUserInfo($ret['access_token'],$ret['openid']);
-        if(!isset($userInfo['nickname'])){
-            $this->returnJson(100);
-        }
-        $this->open_id = $ret['openid'];
-        $this->type = $type;
-        $this->inviter_id = $inviter_id;
-        $this->active_id = $active_id;
-        if($this->login()){
-            $url = $this->host_url ."active/Christmas/index.php";
-            header("location: $url");
-        }
     }
 
     /**
-     * test
+     * web登录
      */
-    public function testAction(){
-        $open_id = htmlspecialchars($_GET['open_id']);
-        $type = intval($_GET['type']);
-        $active_id = intval($_GET['active_id']);
-        $inviter_id = $_GET['inviter_id']?intval($_GET['inviter_id']):0;
-        if(!isset($open_id) || !isset($type) || !isset($active_id) || !$active_id || !$type || !$open_id){
-            $this->returnJson(100);
-        }
-        $this->open_id = $open_id;
-        $this->type = $type;
-        $this->inviter_id = $inviter_id;
-        $this->active_id = $active_id;
-
-//        $this->open_id = $_GET['open_id'];
-//        $this->type = $_GET['type'];
-//        $this->inviter_id = @$_GET['inviter_id'];
-//        $this->active_id = $_GET['active_id'];
-        $this->referer = !empty($_GET['referer'])?$_GET['referer']:"index";
-
-        $result=$this->_login();
-        if($result!==false){
-            $_SESSION['uid'] = $result["uid"];
-            $_SESSION['token'] = $result["token"];
-            $_SESSION['content'] = $result["content"];
-            $url = $this->host_url ."active/Christmas/".$this->referer.".php";
-            header("location: $url");
-        }
-    }
-
-
-    private function _login()
+    private function _webLogin()
     {
         //判断用户是否已经存在
         $userInfo = $this->_isUser();
@@ -91,15 +83,10 @@ class UserroleController extends BaseController
             $content = $userInfo['content'];
         }else{
             //注册
-            $data = [];
-            $data['inviter_id'] = $this->inviter_id ? $this->inviter_id : 0;
-            $data['active_id'] = $this->active_id;
-            $data['open_id'] = $this->open_id;
-            $data['type'] = $this->type;
-            $ret = $this->_regUser($data);
+            $ret = $this->_regUser();
             if($ret){
                 $rand_string = $this->activeUserModel->getRandNum();
-                $token = md5($ret.'@'. $data['open_id'].'@'.$data['type'].'@'.$rand_string);
+                $token = md5($ret.'@'. $this->open_id.'@'.$this->type.'@'.$rand_string);
                 $uid = $ret;
                 $content ='';
             }
@@ -108,51 +95,96 @@ class UserroleController extends BaseController
             $userArr['uid'] = $uid;
             $userArr['token'] = $token;
             $userArr['content'] = $content;
-            return $userArr;
+            $this->returnJson(200, $userArr);
         }else{
-            return false;
+            $this->returnJson(102);
         }
     }
 
-    public function checkAction(){
-        $open_id = htmlspecialchars($_GET['open_id']);
-        $type = intval($_GET['type']);
-        $active_id = intval($_GET['active_id']);
-        $inviter_id = $_GET['inviter_id']?intval($_GET['inviter_id']):0;
-        if(!isset($open_id) || !isset($type) || !isset($active_id) || !$active_id || !$type || !$open_id){
-            $this->returnJson(100);
-        }
-        $this->open_id = $open_id;
-        $this->type = $type;
-        $this->inviter_id = $inviter_id;
-        $this->active_id = $active_id;
+    /**
+     * 微信登录
+     */
+    private function _wenxinLogin()
+    {
+        $code = $this->opend_id;
+        $retArr = $this->_getToken($code);
 
-        $result=$this->_login();
-        if($result!==false){
-            $result['content'] = json_decode($result["content"],true);
-            echo json_encode($result);
+        //判断用户是否已经存在
+        $userInfo = $this->_isUser();
+        if($userInfo){
+            //生成token规则
+            //token = md5(uid@open_id@active_id@type@rand_string)
+            $token = md5($userInfo['id'].'@'. $userInfo['open_id'].'@'.$userInfo['type'].'@'.$userInfo['rand_string']);
+            $uid = $userInfo['id'];
+            $content = $userInfo['content'];
         }else{
-            $result["error"]["errorno"]=100;
-            $result["error"]["errormessage"]="系统错误";
-            echo json_encode($result);
+            //注册
+            $ret = $this->_regUser();
+            if($ret){
+                $rand_string = $this->activeUserModel->getRandNum();
+                $token = md5($ret.'@'. $this->open_id.'@'.$this->type.'@'.$rand_string);
+                $uid = $ret;
+                $content ='';
+            }
+        }
+        if($token && $uid){
+            $userArr['uid'] = $uid;
+            $userArr['token'] = $token;
+            $userArr['content'] = $content;
+            $this->returnJson(200, $userArr);
+        }else{
+            $this->returnJson(102);
         }
     }
 
+    /**
+     * web登录
+     */
+    private function _sinaLogin()
+    {
 
+    }
+
+    /**
+     * web登录
+     */
+    private function _qqLogin()
+    {
+
+    }
+
+//========================================================
+    /**
+     * 用户是否存在
+     * @return mixed
+     */
     private function _isUser()
     {
         //是 返回true 不是返回 false
         return $userinfo = $this->activeUserModel->getUser($this->active_id,$this->type, $this->open_id);
     }
-    private function _regUser($data){
-        return $user = $this->activeUserModel->addUser($data);
+
+    /**
+     * 注册用户
+     * @return mixed
+     */
+    private function _regUser(){
+        $activeUserModel = new activeUserModel();
+        $data = [];
+        $data['inviter_id'] = $this->inviter_id;;
+        $data['active_id'] = $this->active_id;
+        $data['open_id'] = $this->open_id;
+        $data['type'] = $this->type;
+        return $user = $activeUserModel->addUser($data);
     }
 
-    private function _getToken(){
-        $config = Yaf\Application::app()->getConfig();
-        $appid = $config->weixin["appid"];
-        $secret = $config->weixin["appsecret"];
-        $code = htmlspecialchars($_GET['code']);
+    /**
+     * 获取weixin token
+     * @return array
+     */
+    private function _getToken($code){
+        $appid = C("weixin.appid");
+        $secret = C("weixin.appsecret");
         $url ="https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appid&secret=$secret&code=$code&grant_type=authorization_code";
         return $ret = $this->getHttpResponse($url);
     }
@@ -166,11 +198,6 @@ class UserroleController extends BaseController
         $url = "https://api.weixin.qq.com/sns/auth?access_token=$access_token&openid=$openid";
         $ret = $this->getHttpResponse($url);
         return $ret;
-    }
-
-    public function logoutAction(){
-        session_unset();
-        session_destroy();
     }
 
 
