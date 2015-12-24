1b9a240dd7cb804081314ea172fdd48741e27f43
李晨军 <firren@163.com>
2015-12-24 12:03:13 +0800

    add

--- active/test/callback.php
+++ active/test/callback.php
@@ -1,8 +1,26 @@
 <?php
+include_once('base.php');
 /**
  * Created by PhpStorm.
  * User: admin
  * Date: 15/12/24
  * Time: 上午11:47
  */
-var_dump($_GET);
+if($_GET['code']){
+
+    $ret = login();
+    var_dump($ret);
+
+}
+function login(){
+    $code = $_GET['code'];
+    $active_id = 2;
+    $type =1;
+    $login_url = "http://weixin.bbwc.cn/active/public/index.php/index/userrule/login";
+    $post_data = array();
+    $post_data['code'] = $code;
+    $post_data['token'] = $type;
+    $post_data['active_id'] = $active_id;
+    $res = request_post($login_url, $post_data);
+    return $res = json_decode($res, 1);
+}
