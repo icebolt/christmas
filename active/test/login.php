bcd1640f9df55700cc9276b824199effad61eca8
lichenjun <firren@163.com>
2015-12-25 11:07:24 +0800

    test

--- active/test/login.php
+++ active/test/login.php
@@ -12,12 +12,14 @@ $redirect_uri = urlencode('http://weixin.bbwc.cn/active/active/test/callback.php
 $state = "";
 $host = "https://open.weixin.qq.com/connect/oauth2/authorize";
   //   * ?appid=APPID&redirect_uri=REDIRECT_URI&response_type=code&scope=SCOPE&state=STATE#wechat_redirect";
-//$url ="https://open.weixin.qq.com/connect/qrconnect?appid=$appid&redirect_uri=$redirect_uri&response_type=code&scope=snsapi_login&state=STATE#wechat_redirect";
+//我是网页登录
+$url ="https://open.weixin.qq.com/connect/qrconnect?appid=$appid&redirect_uri=$redirect_uri&response_type=code&scope=snsapi_login&state=STATE#wechat_redirect";
 //https://open.weixin.qq.com/connect/oauth2/authorize
 //?appid=wxf0e81c3bee622d60&redirect_uri=http%3A%2F%2Fnba.bluewebgame.com%2Foauth_response.php
 //&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect
 //https://open.weixin.qq.com/connect/oauth2/authorize?appid=APPID&redirect_uri=REDIRECT_URI&response_type=code&scope=SCOPE&state=STATE#wechat_redirect
-$url = $host."?appid=$appid&redirect_uri=$redirect_uri&response_type=code&scope=snsapi_userinfo&state=data#wechat_redirect";
+//我是移动端登录
+//$url = $host."?appid=$appid&redirect_uri=$redirect_uri&response_type=code&scope=snsapi_userinfo&state=data#wechat_redirect";
 header("location:$url;");
 ?>
 
